7ce16c5a808e57bfbabfa52e00e018f88f6fd7e5
ancythomas91 <sayhi2ancy2015@gmail.com>
2023-06-28 22:59:55 +0200

    Refactor package name

--- src/main/java/com/home/application/views/main/MainView.java
+++ src/main/java/com/home/application/views/main/MainView.java
@@ -1,12 +1,18 @@
-package com.example.application.views.main;
+package com.home.application.views.main;
 
+import com.home.application.model.TrainInfo;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.renderer.LitRenderer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @PageTitle("Main")
 @Route(value = "")
@@ -27,6 +33,21 @@ public class MainView extends HorizontalLayout {
         setVerticalComponentAlignment(Alignment.END, name, sayHello);
 
         add(name, sayHello);
+
+
+
+        Grid<TrainInfo> grid = new Grid<>();
+        List<TrainInfo> trainInfos = new ArrayList<>();
+        trainInfos.add(new TrainInfo("Utrecht Centraal","09:23:00","3","SPR", false))
+        trainInfos.add(new TrainInfo("Arnhem Centraal","09:47:00","2","ST", false))
+        trainInfos.add(new TrainInfo("Utrecht Centraal","09:53:00","3","SPR", false))
+
+        grid.setItems(trainInfos);
+
+        grid.addColumn(LitRenderer
+                .<Person>of("<b>[[item.name]]</b>")
+                .withProperty("name", Person::getName)
+        ).setHeader("Name");
     }
 
 }
